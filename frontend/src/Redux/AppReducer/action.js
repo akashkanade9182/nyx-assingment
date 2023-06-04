554348b0619e0e360cfa32f27ff70db1abd08867
akashkanade9182 <akashkanade235@gmail.com>
2023-06-04 13:47:54 +0530

    fina added

--- frontend/src/Redux/AppReducer/action.js
+++ frontend/src/Redux/AppReducer/action.js
@@ -6,7 +6,7 @@ import * as types from "./actionType"
 
 const getAllRecord=()=>(dispatch)=>{
    dispatch({type:types.GET_POST_REQUEST})
-   return axios.get("http://localhost:8000/records/getallrecords").then((r)=>{
+   return axios.get("https://nyx-bakcned1.onrender.com/records/getallrecords").then((r)=>{
     dispatch({type:types.GET_POST_SUCCESS,payload:r.data})
     console.log(r.data)
    }).catch((e)=>{
@@ -16,7 +16,7 @@ const getAllRecord=()=>(dispatch)=>{
 
 const updateRecord=(id,payload,onClose)=>(dispatch)=>{
   dispatch({type:types.PATCH_POST_REQUEST})
-  return axios.patch(`http://localhost:8000/records/update/${id}`,payload).then((r)=>{
+  return axios.patch(`https://nyx-bakcned1.onrender.com/records/update/${id}`,payload).then((r)=>{
     dispatch({type:types.PATCH_POST_SUCCESS,payload:r.data})
    onClose()
   }).catch((e)=>{
@@ -28,7 +28,7 @@ const updateRecord=(id,payload,onClose)=>(dispatch)=>{
 
 const deleteRecord=(id)=>(dispatch)=>{
   dispatch({type:types.DELETE_POST_REQUEST})
-  return axios.delete(`http://localhost:8000/records/delete/${id}`).then((r)=>{
+  return axios.delete(`https://nyx-bakcned1.onrender.com/records/delete/${id}`).then((r)=>{
     dispatch({type:types.DELETE_POST_SUCCESS,payload:r.data})
    
    console.log("udpate data",r.data)
@@ -41,7 +41,7 @@ const deleteRecord=(id)=>(dispatch)=>{
 
 const updateProfilepic=(Id,payload,onClose)=>(dispatch)=>{
   dispatch({type:types.UPDATE_POST_REQUEST})
-  return axios.patch(`http://localhost:8000/records/updatepic/${Id}`,payload).then((r)=>{
+  return axios.patch(`https://nyx-bakcned1.onrender.com/records/updatepic/${Id}`,payload).then((r)=>{
     dispatch({type:types.UPDATE_POST_SUCCESS,payload:r.data})
     console.log(r.data)
     onClose()
